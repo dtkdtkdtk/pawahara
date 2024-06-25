033f3f200de4d678780c074d5a450e53bf5224d9
dtkdtkdtk <takadaibsk4126@gmail.com>
2024-06-25 17:41:23 +0900

    Update pawahara.py

--- pawahara.py
+++ pawahara.py
@@ -71,7 +71,7 @@ if user_input:
         st.chat_message("num").write(len(st.session_state.messages))
         
     # メッセージ数のチェックとリセット
-    if len(st.session_state.messages) >= 12:
+    if len(st.session_state.messages) >= 21:
         st.session_state.messages = [
             {'role': 'system', 'content': '100文字程度で端的に回答して'}
         ]
